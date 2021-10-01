9ba75e08d2be64dd1a1696e61c6d0ea6610e6c1e
kyoya <kyoyaprog@gmail.com>
2021-10-01 21:08:13 +0900

    model層の実装

---
+++ MVVMPractice/Model.swift
@@ -0,0 +1,32 @@
+import RxSwift
+
+enum ModelError: Error {
+    case invalidId
+    case invalidPassword
+    case invalidIdAndPassword
+}
+protocol ModelProtocol {
+    func validate(idText: String?, passwordText: String?) -> Observable<Void>
+}
+final class Model: ModelProtocol {
+    func validate(idText: String?, passwordText: String?) -> Observable<Void> {
+        switch (idText, passwordText) {
+        case (.none, .none):
+            return Observable.error(ModelError.invalidIdAndPassword)
+        case (.none, .some):
+            return Observable.error(ModelError.invalidId)
+        case (.some, .none):
+            return Observable.error(ModelError.invalidPassword)
+        case (let idText?, let passwordText?):
+            switch (idText.isEmpty, passwordText.isEmpty) {
+            case (true, true):
+                return Observable.error(ModelError.invalidIdAndPassword)
+            case (false, false):
+                return Observable.just(())
+            case (true, false):
+                return Observable.error(ModelError.invalidId)
+            case (false, true):
+                return Observable.error(ModelError.invalidPassword)
+            }
+} }
+}
