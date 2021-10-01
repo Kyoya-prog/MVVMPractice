028ea4dc6e98f9d3050740a485147a6bb1383cea
kyoya <kyoyaprog@gmail.com>
2021-10-01 16:46:58 +0900

    Initial Commit

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by 松山響也 on 2021/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
